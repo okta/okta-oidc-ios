7e079f9fc8a7ef7bfd6f7e43511f074a5866ce96
jmelberg <jordan.melberg@okta.com>
2018-03-19 10:13:56 -0700

    :white_check_mark: Adds tests

--- Example/Tests/TestUtils.swift
+++ Example/Tests/TestUtils.swift
@@ -9,11 +9,29 @@
 import AppAuth
 import OktaAuth
 import Vinculum
+import Hydra
 
 struct TestUtils {
-    static let tokenManager = TestUtils.setupMockTokenManager(issuer: "https://demo-org.oktapreview.com/oauth2/default")
+    static let mockIssuer = "https://demo-org.oktapreview.com/oauth2/default"
+    static let mockClientId = "0oae1enia6od2nlz00h7"
+    static let mockScopes = "openid email"
 
-    static func setupMockTokenManager(issuer: String) -> OktaTokenManager {
+    static let mockAccessToken = "abc.123.xyz"
+    static let mockIdToken = "eyJraWQiOiIwWG9xWm1abTVuQlF0UnhUd3E1VDI5czBUenF0RGowenNyOGxGSHA5OHZnIiwiYWxnIjoiUlMyNTYifQ." +
+        "eyJzdWIiOiIwMHVlMWdpMHB0WnBhNjdwVTBoNyIsIm5hbWUiOiJKb3JkYW4gTWVsYmVyZyIsInZlciI6MSwiaXNzIjoiaHR0cHM6Ly9kZW1vLW9yZy" +
+        "5va3RhcHJldmlldy5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiIwb2FlMWVuaWE2b2Qybmx6MDBoNyIsImlhdCI6MTUyMTIzMDMzNiwiZXhwIjox" +
+        "NTIxMjMzOTM2LCJqdGkiOiJJRC5Fc0g5MndqVU1fNTJOdHg1Mlc0QkFpNGVlRUlJak5WbFZYaVZxbkR5S2U4IiwiYW1yIjpbInB3ZCJdLCJpZHAiOi" +
+        "IwMG9lMWdpMG5mU3FBY0VaMjBoNyIsInByZWZlcnJlZF91c2VybmFtZSI6ImpvcmRhbi5tZWxiZXJnQG9rdGEuY29tIiwiYXV0aF90aW1lIjoxNTIx" +
+        "MjMwMzM1LCJhdF9oYXNoIjoiUmNRM2dHeXQ3bHJIckFwenF4RkxmQSJ9.GTrgb19Rb_hJhcKj1NvvMfhkX9_bbn5RNCefH285TEJL0lsmytC60GUiY-" +
+        "MQfI0DkjvBC7Yd2NS4hQyX3YlUn5vWcL6B_cQe-nSR3RD8pvnfmKh28mGrqtn0qfLxq9LmPskKZuXgjBY_hiFpRW-XzGWOgfYp-6t6oDN4LP92bnNNW" +
+        "qNelEI21kePdJQ3fItn3lwhBAFzJ3X35LOJr4-4XAqF0K-dOcpJ_EVyVMUtuQ4s3CD5xfrhYeRbz8d8As7Np7CCqTpWgS1L4AVMvMwdu6QaCo2bMYqH" +
+        "pu1WrZzuBHoQXuDkuYH6xKbKU2bopZGnA8PwrsIbr6PmmTaeH5ww0Q"
+    static let mockRefreshToken = "mockRefreshToken"
+
+    static let tokenManager = TestUtils.setupMockTokenManager(issuer: mockIssuer, options: nil)
+    static let tokenManagerNoValidation = TestUtils.setupMockTokenManager(issuer: mockIssuer, options: ["issuer": mockIssuer])
+
+    static func setupMockTokenManager(issuer: String, options: [String: Any]?) -> Promise<OktaTokenManager> {
         // Creates a mock Okta Token Manager object
         let fooURL = URL(string: issuer)!
         let mockServiceConfig = OIDServiceConfiguration(authorizationEndpoint: fooURL, tokenEndpoint: fooURL)
@@ -29,10 +47,36 @@ struct TestUtils {
                     codeVerifier: nil,
             additionalParameters: nil
         )
-        let mockTokenResponse = OIDTokenResponse(request: mockTokenRequest, parameters: [:])
+
+        let mockTokenResponse = OIDTokenResponse(
+            request: mockTokenRequest,
+            parameters: [
+                "access_token": mockAccessToken as NSCopying & NSObjectProtocol,
+                "expires_in": 300 as NSCopying & NSObjectProtocol,
+                "token_type": "Bearer" as NSCopying & NSObjectProtocol,
+                "id_token": mockIdToken as NSCopying & NSObjectProtocol,
+                "refresh_token": mockRefreshToken as NSCopying & NSObjectProtocol,
+                "scope": mockScopes as NSCopying & NSObjectProtocol
+            ]
+        )
+
         let tempAuthState = OIDAuthState(authorizationResponse: nil, tokenResponse: mockTokenResponse, registrationResponse: nil)
 
-        return OktaTokenManager(authState: tempAuthState, config: [:])
+        return Promise<OktaTokenManager>(in: .background, { resolve, reject, _ in
+            do {
+                let tm = try OktaTokenManager(
+                    authState: tempAuthState,
+                    config: [
+                        "issuer": mockIssuer,
+                        "clientId": mockClientId
+                    ],
+                    validationOptions: options
+                )
+                return resolve(tm)
+            } catch let error {
+                return reject(error)
+            }
+        })
     }
 
     static func getPreviousState() -> OktaTokenManager? {
@@ -45,7 +89,6 @@ struct TestUtils {
             .unarchiveObject(with: encodedAuthState.value) as? OktaTokenManager else {
                 return nil
         }
-
         return previousState
     }
 }
