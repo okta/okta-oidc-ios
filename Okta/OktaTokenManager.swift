a49d913c85d6f64325c5ccc6c9e6d22179eb94ea
anastasiiaiurok-okta <anastasiia.iurok@okta.com>
2019-02-05 11:35:55 +0200

    Reverted changes

--- Okta/OktaTokenManager.swift
+++ Okta/OktaTokenManager.swift
@@ -10,6 +10,8 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
+import OktaAppAuth
+
 open class OktaTokenManager: NSObject, NSCoding {
 
     open var authState: OIDAuthState
