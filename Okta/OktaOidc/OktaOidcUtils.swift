1a7d0f6693a5a2a209793f424805cf5bef4cbef5
Ildar Abdullin <Ildar.Abdullin@okta.com>
2019-04-23 16:54:50 -0700

    Fix user agent

--- Okta/OktaOidc/OktaOidcUtils.swift
+++ Okta/OktaOidc/OktaOidcUtils.swift
@@ -16,7 +16,7 @@ open class OktaOidcUtils: NSObject {
 
     @objc
     open class func userAgentHeader() -> String {
-        return "okta-sdk-appauth-ios/\(OktaOidc.VERSION) iOS/\(UIDevice.current.systemVersion) Device/\(deviceModel())"
+        return "okta-oidc-ios/\(OktaOidc.VERSION) iOS/\(UIDevice.current.systemVersion) Device/\(deviceModel())"
     }
 
     internal class func scrubScopes(_ scopes: String?) -> [String]{
