6a3a33cc2eb29713bb813f6bb543c4d657da2eb4
Ildar Abdullin <Ildar.Abdullin@okta.com>
2019-07-09 11:14:05 -0700

    OKTA-237143-iOS 13 - investigate compatibility with OIDC library

--- Okta/AppAuth/OIDTokenRequest.m
+++ Okta/AppAuth/OIDTokenRequest.m
@@ -15,8 +15,8 @@
         See the License for the specific language governing permissions and
         limitations under the License.
 
- @modifications
- Copyright (C) 2019 Okta Inc.
+    @modifications
+        Copyright (C) 2019 Okta Inc.
 
  */
 
