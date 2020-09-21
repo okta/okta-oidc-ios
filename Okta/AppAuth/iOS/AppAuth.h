492fa3445605f82b73bf1feddd6b0a3da69d2c59
Sam Warner <sam-w@users.noreply.github.com>
2020-09-21 13:02:26 +1000

    generate modulemap for AppAuth target
    # Conflicts:
    #	okta-oidc.xcodeproj/project.pbxproj

---
+++ Okta/AppAuth/iOS/AppAuth.h
@@ -0,0 +1,14 @@
+//
+//  AppAuth.h
+//  AppAuth
+//
+//  Created by Sam Warner on 21/8/20.
+//  Copyright © 2020 Okta. All rights reserved.
+//
+
+#import "AppAuthCore.h"
+#import "OIDAuthorizationService+IOS.h"
+#import "OIDAuthState+IOS.h"
+#import "OIDExternalUserAgentIOS.h"
+#import "OIDExternalUserAgentIOSCustomBrowser.h"
+#import "OIDExternalUserAgentNoSsoIOS.h"
---
+++ Okta/AppAuth/macOS/AppAuth.h
@@ -0,0 +1,14 @@
+//
+//  AppAuth.h
+//  AppAuth-mac
+//
+//  Created by Sam Warner on 21/8/20.
+//  Copyright © 2020 Okta. All rights reserved.
+//
+
+#import "AppAuthCore.h"
+#import "OIDAuthorizationService+Mac.h"
+#import "OIDAuthState+Mac.h"
+#import "OIDExternalUserAgentMac.h"
+#import "OIDRedirectHTTPHandler.h"
+#import "LoopbackHTTPServer/OIDLoopbackHTTPServer.h"
